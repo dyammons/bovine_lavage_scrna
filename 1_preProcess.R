9269c5d41baf587d4a9f4cd0f969ac070373e5fa
dyammons <dyammons95@gmail.com>
2024-03-07 19:29:08 -0700

    Finish run of preprocessing
    The intial job error'd out and needed to be
    restarted. This job skips step 1 (since it was
    already run) and skipped clustree to save time.
    
    JOBID: 5261956

--- 1_preProcess.R
+++ 1_preProcess.R
@@ -8,25 +8,25 @@ source("/pl/active/dow_lab/dylan/repos/scrna-seq/analysis-code/customFunctions_S
 ######################################## <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 outName <- "20240307_bov_lav_n5n5_dxVSh"
 
-load10x(din = "../input/", dout = "../output/s1/", outName = outName, testQC = F,
-                    nFeature_RNA_high = 6500, nFeature_RNA_low = 200, 
-                    nCount_RNA_high = 40000, nCount_RNA_low = 100, 
-                    percent.mt_high = 1, mt_pattern = "^MT-",
-                    nfeatures = 2000,
-                    removeDubs = TRUE, removeRBC_pal = FALSE, 
-                    pal_feats = NULL, isolatePalRBC = FALSE,
-                    featPlots = c("PTPRC", "CD3E", "CD8A", "GZMA", 
-                                  "IL7R", "ANPEP", "FLT3", "DLA-DRA", 
-                                  "CD4", "MS4A1", "PPBP","HBM")
-                   )
+# load10x(din = "../input/", dout = "../output/s1/", outName = outName, testQC = F,
+#                     nFeature_RNA_high = 6500, nFeature_RNA_low = 200, 
+#                     nCount_RNA_high = 40000, nCount_RNA_low = 100, 
+#                     percent.mt_high = 1, mt_pattern = "^MT-",
+#                     nfeatures = 2000,
+#                     removeDubs = TRUE, removeRBC_pal = FALSE, 
+#                     pal_feats = NULL, isolatePalRBC = FALSE,
+#                     featPlots = c("PTPRC", "CD3E", "CD8A", "GZMA", 
+#                                   "IL7R", "ANPEP", "FLT3", "DLA-DRA", 
+#                                   "CD4", "MS4A1", "PPBP","HBM")
+#                    )
 
 #integrate the data using all of the four Seurat v5 integration methods
 seu.obj <- integrateData(din = "../output/s1/", dout = "../output/s2/", outName = outName, normalization.method = "LogNormalize",
                          runAllMethods = TRUE, indReClus = F)
 
-#use clustree to identify clustering parameters that appear most appropriate
-clusTree(seu.obj = seu.obj, dout = "../output/clustree/", outName = outName, 
-            test_dims = c(50,45,40), algorithm = 3, prefix = "integrated_snn_res.")
+# #use clustree to identify clustering parameters that appear most appropriate
+# clusTree(seu.obj = seu.obj, dout = "../output/clustree/", outName = outName, 
+#             test_dims = 45, algorithm = 3, prefix = "integrated_snn_res.")
 
 #complete data visualization
 for (x in list("integrated.cca", "integrated.harmony", "integrated.joint", "integrated.rcpa")) {
